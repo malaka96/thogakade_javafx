fc845cdc13a798c082963505b44a2bcf8f20dfad
malaka96 <madhubhashana655@gmail.com>
2025-10-11 06:51:31 +0530

    login page created

---
+++ src/main/java/controllers/LoginFormController.java
@@ -0,0 +1,55 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginFormController {
+
+    final String email = "m@gmail.com";
+    final String password = "123123";
+
+    @FXML
+    private Button LoginBtn;
+
+    @FXML
+    private TextField emailTf;
+
+    @FXML
+    private TextField passwordTf;
+
+    public void LoginOA(ActionEvent actionEvent) {
+        String em = emailTf.getText();
+        String ps = passwordTf.getText();
+
+        if (em.isEmpty() || ps.isEmpty()) {
+            new Alert(Alert.AlertType.ERROR,"Please enter both email and password").show();
+        }
+
+        if (em.equals(email) && ps.equals(password)) {
+
+
+            Stage stage = new Stage();
+            try {
+                stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/views/user.fxml"))));
+            } catch (
+                    IOException e) {
+                throw new RuntimeException(e);
+            }
+            stage.show();
+            new Alert(Alert.AlertType.INFORMATION, "Login successful!").show();
+        } else {
+            new Alert(Alert.AlertType.ERROR, "Invalid email or password.").show();
+        }
+
+
+
+    }
+}
